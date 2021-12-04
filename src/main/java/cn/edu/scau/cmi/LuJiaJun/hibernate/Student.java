3e8ee4f9c9ff1b6a3e9359d5ed0046a661819b60
Lychee <Lychee@qq.com>
2021-12-04 16:16:56 +0800

    First commit！

---
+++ src/main/java/cn/edu/scau/cmi/LuJiaJun/hibernate/Student.java
@@ -0,0 +1,14 @@
+package cn.edu.scau.cmi.LuJiaJun.hibernate;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Student {
+    private int sid;
+    private String studentName;
+}
---
+++ src/main/java/cn/edu/scau/cmi/LuJiaJun/hibernate/Teacher.java
@@ -0,0 +1,15 @@
+package cn.edu.scau.cmi.LuJiaJun.hibernate;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Teacher {
+
+    private int tid;
+    private String teacherName;
+
+}
